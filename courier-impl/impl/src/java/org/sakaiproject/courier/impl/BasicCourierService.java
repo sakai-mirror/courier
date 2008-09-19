36e2b436204a57f7b6fe04e23e153b0136d68a2f
stephen.marquard@uct.ac.za <stephen.marquard@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-09-19 10:31:10 +0000

    SAK-11875 Merge branch version (equivalent to trunk) to 2-5-x
    git-svn-id: https://source.sakaiproject.org/svn/courier/branches/sakai_2-5-x@52501 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- courier-impl/impl/src/java/org/sakaiproject/courier/impl/BasicCourierService.java
+++ courier-impl/impl/src/java/org/sakaiproject/courier/impl/BasicCourierService.java
@@ -21,12 +21,13 @@
 
 package org.sakaiproject.courier.impl;
 
-// imports
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -42,10 +43,28 @@ import org.sakaiproject.util.StringUtil;
 public class BasicCourierService implements CourierService
 {
 	/** Our logger. */
-	private static Log M_log = LogFactory.getLog(BasicCourierService.class);
+	private static final Log M_log = LogFactory.getLog(BasicCourierService.class);
+
+        protected static final int nLocks = 100;
 
 	/** Stores a List of Deliveries for each address, keyed by address. */
-	protected Map m_addresses = new Hashtable();
+	protected Map<String, List<Delivery>> m_addresses =
+	    new ConcurrentHashMap<String, List<Delivery>>(nLocks, 0.75f, nLocks);
+
+       /** 
+	** Array of objects for locking. You take the hash MOD the size of the
+	** array to find the element in the array to lock. This allows better
+	** concurrency than a global lock on the hash table. I still use
+	** ConcurrentHashMap because otherwise I worry about two attempts
+	** to add an entry that happen to use the same lock. So I'm 
+	** depending upon ConcurrentHashMap to maintain the soundness of
+	** the data structure. Unfortunately the synchronization in it doesn't
+	** help against situations where I get a value and then test or
+	** modify it. That needs real locks.
+	**/
+
+        protected Object[] locks;
+
 
 	/**********************************************************************************************************************************************************************************************************************************************************
 	 * Dependencies and their setter methods
@@ -60,8 +79,11 @@ public class BasicCourierService implements CourierService
 	 */
 	public void init()
 	{
-		m_addresses.clear();
 		M_log.info("init()");
+		m_addresses.clear();
+		locks = new Object[nLocks];
+		for (int i = 0; i < nLocks; i++)
+		    locks[i] = new Object();
 	}
 
 	/**
@@ -69,55 +91,58 @@ public class BasicCourierService implements CourierService
 	 */
 	public void destroy()
 	{
-		m_addresses.clear();
 		M_log.info("destroy()");
+		m_addresses.clear();
+		locks = null;
 	}
 
 	/**********************************************************************************************************************************************************************************************************************************************************
 	 * CourierService implementation
 	 *********************************************************************************************************************************************************************************************************************************************************/
 
+         protected int slot(String s) {
+	     
+	     int hash = Math.abs(s.hashCode());
+
+	     return hash % nLocks;
+	 }
+
 	/**
-	 * Queue up a delivery for the client window identified in the Delivery object. The particular form of delivery is determined by the type of Delivery object sent.
+	 * Queue up a delivery for the client window identified in the Delivery 
+	 * object. The particular form of delivery is determined by the type of 
+	 * Delivery object sent.
 	 * 
 	 * @param delivery
 	 *        The Delivery (or extension) object to deliver.
 	 */
 	public void deliver(Delivery delivery)
 	{
-		if (M_log.isDebugEnabled()) M_log.debug("deliver(): " + delivery);
+		if (M_log.isDebugEnabled())
+			M_log.debug("deliver(Delivery " + delivery + ")");
 
-		String address = delivery.getAddress();
+		final String address = delivery.getAddress();
 
-		// find the entry in m_addresses
-		List deliveries = (List) m_addresses.get(address);
+		synchronized(locks[slot(address)]) {
 
-		// create if needed
-		if (deliveries == null)
-		{
-			synchronized (m_addresses)
-			{
-				deliveries = (List) m_addresses.get(address);
-				if (deliveries == null)
-				{
-					deliveries = new Vector();
-					m_addresses.put(address, deliveries);
-				}
-			}
-		}
+		    // find the entry in m_addresses
+		    List<Delivery> deliveries = m_addresses.get(address);
 
-		// if this doesn't exist in the list already, add it
-		synchronized (deliveries)
-		{
-			if (!deliveries.contains(delivery))
-			{
-				deliveries.add(delivery);
-			}
+		    // create if needed
+		    if (deliveries == null) {
+				deliveries = new ArrayList<Delivery>();
+				m_addresses.put(address, deliveries);
+		    }
+
+		    // if this doesn't exist in the list already, add it
+		    if (!deliveries.contains(delivery)) {
+		    	deliveries.add(delivery);
+		    }
 		}
 	}
 
 	/**
-	 * Clear any pending delivery requests to the particular client window for this element.
+	 * Clear any pending delivery requests to the particular client window for 
+	 * this element.
 	 * 
 	 * @param address
 	 *        The address of the client window.
@@ -126,34 +151,30 @@ public class BasicCourierService implements CourierService
 	 */
 	public void clear(String address, String elementId)
 	{
-		if (M_log.isDebugEnabled()) M_log.debug("clear(): " + address + ", " + elementId);
+		if (M_log.isDebugEnabled())
+			M_log.debug("clear(String " + address + ", String " + elementId
+					+ ")");
 
-		// find the entry in m_addresses
-		List deliveries = (List) m_addresses.get(address);
+		synchronized(locks[slot(address)]) {
+		    
+		    // find the entry in m_addresses
+		    List<Delivery> deliveries = m_addresses.get(address);
+		    
+		    // if not there we are done
+		    if (deliveries == null) return;
 
-		// if not there we are done
-		if (deliveries == null) return;
-
-		// remove any Deliveries with this elementId
-		synchronized (deliveries)
-		{
-			for (Iterator it = deliveries.iterator(); it.hasNext();)
-			{
-				Delivery delivery = (Delivery) it.next();
-				if (!StringUtil.different(delivery.getElement(), elementId))
-				{
-					it.remove();
-				}
+		    // remove any Deliveries with this elementId
+		    for (Iterator<Delivery> it = deliveries.iterator(); it.hasNext();) {
+			Delivery delivery = it.next();
+			if (!StringUtil.different(delivery.getElement(), elementId)) {
+			    it.remove();
 			}
-		}
+		    }
 
-		// if none left, remove it from the list
-		if (deliveries.isEmpty())
-		{
-			synchronized (m_addresses)
-			{
-				m_addresses.remove(address);
-			}
+		    // if none left, remove it from the list
+		    if (deliveries.isEmpty()) {
+			m_addresses.remove(address);
+		    }
 		}
 	}
 
@@ -165,12 +186,12 @@ public class BasicCourierService implements CourierService
 	 */
 	public void clear(String address)
 	{
-		if (M_log.isDebugEnabled()) M_log.debug("clear(): " + address);
+		if (M_log.isDebugEnabled())
+			M_log.debug("clear(String " + address + ")");
 
-		// remove this portal from m_addresses
-		synchronized (m_addresses)
-		{
-			m_addresses.remove(address);
+		synchronized(locks[slot(address)]) {
+		    // remove this portal from m_addresses
+		    m_addresses.remove(address);
 		}
 	}
 
@@ -181,52 +202,62 @@ public class BasicCourierService implements CourierService
 	 *        The address of client window.
 	 * @return a List of Delivery objects addressed to this session client window.
 	 */
+	@SuppressWarnings("unchecked")
 	public List getDeliveries(String address)
 	{
-		if (M_log.isDebugEnabled()) M_log.debug("getDeliveries(): " + address);
+		if (M_log.isDebugEnabled())
+			M_log.debug("getDeliveries(String " + address + ")");
 
-		// find the entry in m_addresses
-		List deliveries = null;
-		synchronized (m_addresses)
-		{
-			deliveries = (List) m_addresses.get(address);
-			if (deliveries != null)
-			{
-				m_addresses.remove(address);
-			}
+		List<Delivery> deliveries;
+
+		synchronized(locks[slot(address)]) {
+
+		    // find the entry in m_addresses
+		    deliveries = m_addresses.get(address);
+		    if (deliveries == null) { // if null, return something
+		    	return Collections.EMPTY_LIST; // this should return null!
+		    } else {
+		    	m_addresses.remove(address);
+		    }
 		}
 
-		// if empty, return something
-		if (deliveries == null)
-		{
-			deliveries = new Vector();
-		}
+		// do this outside of the sync. no reason to hold
+		// other operations now that we've atomically gotten the list
+		// I'm worried about delays and maybe even deadlocks if we
+		// do arbitrary code while holding a lock.
 
-		synchronized (deliveries)
-      {
-   		// "act" all the deliveries
-   		for (Iterator it = deliveries.iterator(); it.hasNext();)
-   		{
-   			Delivery delivery = (Delivery) it.next();
-   			delivery.act();
-   		}
-      }
+		// "act" all the deliveries
+		for (Delivery delivery : deliveries)
+		    {
+			delivery.act();
+		    }
+
 		return deliveries;
 	}
 
 	/**
-	 * Check to see if there are any deliveries queued up for a particular session client window.
+	 * Check to see if there are any deliveries queued up for a particular
+	 * session client window.
 	 * 
 	 * @param address
-	 *        The address of the client window.
-	 * @return true if there are deliveries for this client window, false if not.
+	 *            The address of the client window.
+	 * @return true if there are deliveries for this client window, false if
+	 *         not.
+         *
+	 * WARNING: This method is almost certainly not what you want.
+	 * I can see no way to use it that won't have synchronization problems.
 	 */
 	public boolean hasDeliveries(String address)
 	{
-		if (M_log.isDebugEnabled()) M_log.debug("hasDeliveries(): " + address);
+		if (M_log.isDebugEnabled())
+			M_log.debug("hasDeliveries(String " + address + ")");
+
+		List<Delivery> deliveries;
 
 		// find the entry in m_addresses
-		List deliveries = (List) m_addresses.get(address);
+		synchronized(locks[slot(address)]) {
+		    deliveries = m_addresses.get(address);
+		}
 		if (deliveries == null) return false;
 
 		return (!deliveries.isEmpty());
